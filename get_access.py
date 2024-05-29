58c4a590b0818e3afc7c0e94ec1fb15a86a53a36
Fatemeh <fatemeh1725@yahoo.com>
2024-05-29 19:58:05 +0330

    second update

--- get_access.py
+++ get_access.py
@@ -2,28 +2,35 @@ import os
 import requests
 import base64
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv, set_key
+from pathlib import Path
 
-load_dotenv()
+def get_access_key():
+    load_dotenv()
 
-client_id = os.getenv('Client_ID')
-client_secret = os.getenv('Client_secret')
+    client_id = os.getenv('Client_ID')
+    client_secret = os.getenv('Client_secret')
 
-# Encode client ID and client secret
-client_creds = f"{client_id}:{client_secret}"
-client_creds_b64 = base64.b64encode(client_creds.encode('utf-8'))
-# Get the token
-token_url = "https://accounts.spotify.com/api/token"
-token_data = {
-    "grant_type": "client_credentials"
-}
-token_headers = {
-    "Authorization": f"Basic {str(client_creds_b64, 'utf-8')}",
-    "Content-Type": "application/x-www-form-urlencoded"
-}
+    # Encode client ID and client secret
+    client_creds = f"{client_id}:{client_secret}"
+    client_creds_b64 = base64.b64encode(client_creds.encode('utf-8'))
+    # Get the token
+    token_url = "https://accounts.spotify.com/api/token"
+    token_data = {
+        "grant_type": "client_credentials"
+    }
+    token_headers = {
+        "Authorization": f"Basic {str(client_creds_b64, 'utf-8')}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
 
-r = requests.post(token_url, data=token_data, headers=token_headers)
-token_response_data = r.json()
-access_token = token_response_data['access_token']
+    r = requests.post(token_url, data=token_data, headers=token_headers)
+    token_response_data = r.json()
+    access_token = token_response_data['access_token']
 
-print("acc: ", access_token)
+    env_file_path = Path(".env")
+    load_dotenv(dotenv_path=env_file_path)
+    set_key(dotenv_path=env_file_path, key_to_set="ACCESS_TOKEN", value_to_set=str(access_token))
+        
+# print("acc: ", access_token)
+# get_access_key()
--- get_details.py
+++ get_details.py
@@ -1,25 +1,34 @@
+import os
 import requests
 import pandas as pd
 
-access_token = 'BQB9bOxlCq-dQQ9dwkPqTqmHETBh5ZA7x6m7NO3IwKtldvnbtQZlrzc3EB5srV39YdgL01CU5dXMSoA3FatNcWI0YqRCrRtUsq95TLJ3-Gz-aXVqiUg'
+from dotenv import load_dotenv
 
+load_dotenv()
+access_token = os.getenv('ACCESS_TOKEN')
 
 # Function to fetch multiple tracks' details and audio features
 def get_tracks_details_and_audio_features(track_ids, access_token):
     headers = {"Authorization": f"Bearer {access_token}"}
     ids = ','.join(track_ids)
     
-    # Fetch track details
+    # Fetch track details and audio features
     tracks_url = f"https://api.spotify.com/v1/tracks?ids={ids}"
-    tracks_response = requests.get(tracks_url, headers=headers)
-    tracks_details = tracks_response.json()['tracks']
-    
-    # Fetch track audio features
     audio_features_url = f"https://api.spotify.com/v1/audio-features?ids={ids}"
-    audio_features_response = requests.get(audio_features_url, headers=headers)
-    tracks_audio_features = audio_features_response.json()['audio_features']
     
-    return tracks_details, tracks_audio_features
+    tracks_response = requests.get(tracks_url, headers=headers)
+    audio_features_response = requests.get(audio_features_url, headers=headers)
+    
+    if tracks_response == 200 and audio_features_response == 200:
+        tracks_details = tracks_response.json()['tracks']
+        tracks_audio_features = audio_features_response.json()['audio_features']
+    
+        return tracks_details, tracks_audio_features
+    elif tracks_response == 429 and audio_features_response == 429:
+        ra1 = int(tracks_response.headers.get('Retry-After', 1))
+        ra2 = int(tracks_response.headers.get('Retry-After', 1))
+        
+        return max(ra1, ra2)
 
 # Example track IDs (add up to 50 track IDs here)
 track_ids = ['7221xIgOnuakPdLqT0F3nP', '629DixmZGHc7ILtEntuiWE', '2qSkIjg1o9h3YT9RAgYN75', '4IadxL6BUymXlh8RCJJu7T', '2FQrifJ1N335Ljm3TjTVVf', '2OzhQlSqBEmt7hmkYxfT6m', '7fzHQizxTqy8wTXwlrgPQQ', '6tNQ70jh4OwmPGpYy6R2o9',
@@ -54,7 +63,7 @@ for track, features in zip(tracks_details, tracks_audio_features):
 df = pd.DataFrame(data)
 
 # Save the DataFrame to a CSV file
-df.to_csv('spotify_tracks.csv', index=False)
+# df.to_csv('spotify_tracks.csv', index=False)
 
 print(f"Saved {len(df)} tracks to 'spotify_tracks.csv'")
 
