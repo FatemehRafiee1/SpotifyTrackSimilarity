a829cb21fed9ef2d3a6d79b47f57f5ed86bc2cf5
Fatemeh <Fatemeh1725@yahoo.com>
2024-05-29 22:18:32 +0330

    give track_ids and store in each iteration

--- get_details.py
+++ get_details.py
@@ -1,11 +1,13 @@
 import os
-import requests
+import logging
 import pandas as pd
-
 from dotenv import load_dotenv
+
+from utils import get_response
 
 load_dotenv()
 access_token = os.getenv('ACCESS_TOKEN')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Function to fetch multiple tracks' details and audio features
 def get_tracks_details_and_audio_features(track_ids, access_token):
@@ -16,54 +18,26 @@ def get_tracks_details_and_audio_features(track_ids, access_token):
     tracks_url = f"https://api.spotify.com/v1/tracks?ids={ids}"
     audio_features_url = f"https://api.spotify.com/v1/audio-features?ids={ids}"
     
-    tracks_response = requests.get(tracks_url, headers=headers)
-    audio_features_response = requests.get(audio_features_url, headers=headers)
-    
-    if tracks_response == 200 and audio_features_response == 200:
-        tracks_details = tracks_response.json()['tracks']
-        tracks_audio_features = audio_features_response.json()['audio_features']
-    
-        return tracks_details, tracks_audio_features
-    elif tracks_response == 429 and audio_features_response == 429:
-        ra1 = int(tracks_response.headers.get('Retry-After', 1))
-        ra2 = int(tracks_response.headers.get('Retry-After', 1))
-        
-        return max(ra1, ra2)
+    tracks_details = get_response(tracks_url, headers)['tracks']
+    tracks_audio_features = get_response(audio_features_url, headers)['audio_features']
 
-# Example track IDs (add up to 50 track IDs here)
-track_ids = ['7221xIgOnuakPdLqT0F3nP', '629DixmZGHc7ILtEntuiWE', '2qSkIjg1o9h3YT9RAgYN75', '4IadxL6BUymXlh8RCJJu7T', '2FQrifJ1N335Ljm3TjTVVf', '2OzhQlSqBEmt7hmkYxfT6m', '7fzHQizxTqy8wTXwlrgPQQ', '6tNQ70jh4OwmPGpYy6R2o9',
-             '46kspZSY3aKmwQe7O77fCC', '1bjeWoagtHmUKputLVyDxQ', '6AI3ezQ4o3HUoP6Dhudph3', '2GxrNKugF82CnoRFbQfzPf', '17phhZDn6oGtzMe56NuWvj', '7BRD7x5pt8Lqa1eGYC4dzj', '0WbMK4wrZ1wFSty9F7FCgu', '4NJqhmkGN042BrvHoMKUrJ', '3qhlB30KknSejmIvZZLjOD', '4q5YezDOIPcoLr8R81x9qy', '5uQ7de4EWjb3rkcFxyEOpu', '51eSHglvG1RJXtL3qI5trr', '3rUGC1vUpkDG9CZFHMur1t', '0mflMxspEfB0VbI1kyLiAv', '0Z7nGFVCLfixWctgePsRk9']
+    return tracks_details, tracks_audio_features
 
 
-# Fetch track details and audio features
-tracks_details, tracks_audio_features = get_tracks_details_and_audio_features(track_ids, access_token)
+id_df = pd.read_csv('track_ids_2.csv')
+track_ids = id_df.track_id.values
+res_df = pd.DataFrame(columns=['Track ID', 'Track Name', 'Artist Name', 'Danceability', 'Energy', 'Key',
+                                'Loudness','Mode', 'Speechiness', 'Acousticness', 'Instrumentalness', 'Liveness',
+                                'Valence', 'Tempo', 'Duration (ms)'])
+size = 2
+for i in range(size, len(id_df), size):
+    tracks_details, tracks_audio_features = get_tracks_details_and_audio_features(track_ids[i-size:i], access_token)
 
-# Prepare the data for the DataFrame
-data = []
-for track, features in zip(tracks_details, tracks_audio_features):
-    track_info = {
-        'Track Name': track['name'],
-        'Artist Name': track['artists'][0]['name'],
-        'Danceability': features['danceability'],
-        'Energy': features['energy'],
-        'Key': features['key'],
-        'Loudness': features['loudness'],
-        'Mode': features['mode'],
-        'Speechiness': features['speechiness'],
-        'Acousticness': features['acousticness'],
-        'Instrumentalness': features['instrumentalness'],
-        'Liveness': features['liveness'],
-        'Valence': features['valence'],
-        'Tempo': features['tempo'],
-        'Duration (ms)': features['duration_ms']
-    }
-    data.append(track_info)
+    for track, features in zip(tracks_details, tracks_audio_features):
+        track_info = [track['id'],track['name'],track['artists'][0]['name'],features['danceability'],features['energy'],
+                      features['key'], features['loudness'], features['mode'], features['speechiness'], features['acousticness'],
+                      features['instrumentalness'], features['liveness'], features['valence'], features['tempo'], features['duration_ms']]
 
-# Create a DataFrame
-df = pd.DataFrame(data)
-
-# Save the DataFrame to a CSV file
-# df.to_csv('spotify_tracks.csv', index=False)
-
-print(f"Saved {len(df)} tracks to 'spotify_tracks.csv'")
-
+    res_df.loc[len(res_df)] = track_info
+    res_df.to_csv('test.csv', index=False)
+    logging.info(f"saved track {track['id']} ({track['name']}), i : {i}")
